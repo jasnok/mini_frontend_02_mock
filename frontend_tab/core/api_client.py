9ecb61c6494fb5999267ee2dea2ae6da45d4d12e
jasnok <jso46034@naver.com>
2026-07-30 14:12:43 +0900

    260730_third

--- frontend_tab/core/api_client.py
+++ frontend_tab/core/api_client.py
@@ -7,7 +7,7 @@ import httpx
 
 
 # BACKEND_URL = "http://127.0.0.1:800"
-BACKEND_URL = "https://mini-frontend-02-mock-ca36.onrender.com"
+BACKEND_URL = "https://mini-frontend-02-mock-73qg.onrender.com"
 
 REQUEST_TIMEOUT = 10.0
 
